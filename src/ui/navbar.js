b753388c6ca9eca767d3a1e28d6937552058fa20
Joe Meszaros <jmeszaros@hiya.com>
2024-12-27 14:22:55 +0100

    Reformat navbar, change glyphicons

--- src/ui/navbar.js
+++ src/ui/navbar.js
@@ -1,157 +1,213 @@
-import { Exporter } from "../io/export.js";
+import { Exporter } from '../io/export.js';
 
 class NavigationBar {
 
-    /**
-     * 
-     * @param {HTMLElement} domElement - The HTML DOM element of the navigation bar
-     * @param {Map<String, Map>} options - Global project options, like global visibility of an object
-     * @param {MyScene} scene - The 3D scene
-     */
-    constructor(db, domElement, options, scene) {
-        this.db = db;
-        this.options = options;
-        this.scene = scene;
-        this.#buildNavbar(domElement);
-        this.#addNavbarClickListener();
-    }
+  /**
+   *
+   * @param {HTMLElement} domElement - The HTML DOM element of the navigation bar
+   * @param {Map<String, Map>} options - Global project options, like global visibility of an object
+   * @param {MyScene} scene - The 3D scene
+   */
+  constructor(db, domElement, options, scene) {
+    this.db = db;
+    this.options = options;
+    this.scene = scene;
+    this.#buildNavbar(domElement);
+    this.#addNavbarClickListener();
+  }
 
-    #getMenus() {
-        return [
-            {
-                name: "File", elements: [
-                    { "name": "Open TopoDroid file", "click": function () { document.getElementById('topodroidInput').click(); } },
-                    { "name": "Open Polygon file", "click": function () { document.getElementById('polygonInput').click(); } },
-                    { "name": "Open JSON file", "click": function () { document.getElementById('jsonInput').click(); } },
-                    { "name": "Export JSON", "click":  () => Exporter.exportCaves(this.db.caves) },
-                    { "name": "Export PNG", "click":  () => Exporter.exportPNG(this.scene) }
-                ]
-            } , {
-                name: "Surface", elements: [
-                    { "name": "Open PLY file", "click": function () { document.getElementById('plyInput').click(); } },
-                ]
+  #getMenus() {
+    return [
+      {
+        name     : 'File',
+        elements : [
+          {
+            name  : 'Open TopoDroid file',
+            click : () => {
+              document.getElementById('topodroidInput').click();
             }
+          },
+          {
+            name  : 'Open Polygon file',
+            click : function () {
+              document.getElementById('polygonInput').click();
+            }
+          },
+          {
+            name  : 'Open JSON file',
+            click : function () {
+              document.getElementById('jsonInput').click();
+            }
+          },
+          { name: 'Export JSON', click: () => Exporter.exportCaves(this.db.caves) },
+          { name: 'Export PNG', click: () => Exporter.exportPNG(this.scene) }
         ]
-    }
-
-    #getIcons() {
-        return [
-            {
-                "tooltip": "Zoom to fit", "glyphName": "fullscreen", "click": () => this.scene.fitScene()
-            },
-            {
-                "tooltip": "Zoom in", "glyphName": "zoom-in", "click": () => this.scene.zoomWithStep(this.options.scene.zoomStep)
-            },
-            {
-                "tooltip": "Zoom out", "glyphName": "zoom-out", "click": () => this.scene.zoomWithStep(-1 * this.options.scene.zoomStep)
-            },
-            {
-                "tooltip": "Plan", "icon": "./icons/plan.svg", "click": () => this.scene.lookAtPlan()
-            },
-            {
-                "tooltip": "Profile", "icon": "./icons/profile.svg", "click": () => this.scene.lookAtProfile()
-            },
-            {
-                "tooltip": "3D", "icon": "./icons/3d.svg", "click": () => this.scene.lookAt3D()
-            },
-            {
-                "tooltip": "Bounding box", "icon": "./icons/bounding_box.svg", "click": () => this.scene.toogleBoundingBox()
-            },
-            {
-                "tooltip": "Show beddings", "icon": "./icons/bedding.svg", "click": () => this.scene.tooglePlaneFor("bedding")
-            },
-            {
-                "tooltip": "Show faults", "icon": "./icons/fault.svg", "click": () => this.scene.tooglePlaneFor("fault")
-            },
-            {
-                "tooltip": "Line color mode", "icon": "./icons/cl_color.svg", "click": () => this.scene.rollCenterLineColor()
-            },
-            {
-                "tooltip": "Grid position/visibility", "icon": "./icons/grid.svg", "click": () => this.scene.grid.roll()
-            },
-            {
-                "tooltip": "Surface visibility", "icon": "./icons/surface.svg", "click": () => this.scene.rollSurface()
-            },            
-
+      },
+      {
+        name     : 'Surface',
+        elements : [
+          {
+            name  : 'Open PLY file',
+            click : function () {
+              document.getElementById('plyInput').click();
+            }
+          }
         ]
-    }
+      }
+    ];
+  }
 
-    #addNavbarClickListener() {
-        //Close the dropdown if the user clicks outside of it
-        window.onclick = function (e) {
-            if (!e.target.matches('.dropbtn')) {
-                document.querySelectorAll('.mydropdown-content').forEach((c) => {
-                    if (c.classList.contains('mydropdown-show')) {
-                        c.classList.remove('mydropdown-show');
-                    }
-                });
-            }
-        }
-    }
+  #getIcons() {
+    return [
+      {
+        tooltip : 'Zoom to fit',
+        icon    : './icons/zoom_fit.svg',
+        click   : () => this.scene.fitScene()
+      },
+      {
+        tooltip : 'Zoom in',
+        icon    : './icons/zoom_in.svg',
+        click   : () => this.scene.zoomWithStep(this.options.scene.zoomStep)
+      },
+      {
+        tooltip : 'Zoom out',
+        icon    : './icons/zoom_out.svg',
+        click   : () => this.scene.zoomWithStep(-1 * this.options.scene.zoomStep)
+      },
+      {
+        tooltip : 'Plan',
+        icon    : './icons/plan.svg',
+        click   : () => this.scene.lookAtPlan()
+      },
+      {
+        tooltip : 'Profile',
+        icon    : './icons/profile.svg',
+        click   : () => this.scene.lookAtProfile()
+      },
+      {
+        tooltip : '3D',
+        icon    : './icons/3d.svg',
+        click   : () => this.scene.lookAt3D()
+      },
+      {
+        tooltip : 'Bounding box',
+        icon    : './icons/bounding_box.svg',
+        click   : () => this.scene.toogleBoundingBox()
+      },
+      {
+        tooltip : 'Show beddings',
+        icon    : './icons/bedding.svg',
+        click   : () => this.scene.tooglePlaneFor('bedding')
+      },
+      {
+        tooltip : 'Show faults',
+        icon    : './icons/fault.svg',
+        click   : () => this.scene.tooglePlaneFor('fault')
+      },
+      {
+        tooltip : 'Line color mode',
+        icon    : './icons/cl_color.svg',
+        click   : () => this.scene.rollCenterLineColor()
+      },
+      {
+        tooltip : 'Grid position/visibility',
+        icon    : './icons/grid.svg',
+        click   : () => this.scene.grid.roll()
+      },
+      {
+        tooltip : 'Surface visibility',
+        icon    : './icons/surface.svg',
+        click   : () => this.scene.rollSurface()
+      }
 
-    #buildNavbar(navbarHtmlElement) {
-        const createMenu = (name, elements) => {
-            const c = document.createElement('div');
-            c.setAttribute("class", "mydropdown-content");
-            c.setAttribute("id", "myDropdown");
+    ];
+  }
 
-            elements.forEach((e) => {
-                const a = document.createElement('a');
-                a.appendChild(document.createTextNode(e.name))
-                a.onclick = e.click;
-                c.appendChild(a);
-            });
+  #addNavbarClickListener() {
+    //Close the dropdown if the user clicks outside of it
+    window.onclick = function (e) {
+      if (!e.target.matches('.dropbtn')) {
+        document.querySelectorAll('.mydropdown-content').forEach((c) => {
+          if (c.classList.contains('mydropdown-show')) {
+            c.classList.remove('mydropdown-show');
+          }
+        });
+      }
+    };
+  }
 
-            const d = document.createElement('div');
-            d.setAttribute("class", "mydropdown")
-            const b = document.createElement('button');
-            b.setAttribute("class", "dropbtn");
-            b.onclick = function () {
-                c.classList.toggle("mydropdown-show");
-                document.querySelectorAll('.mydropdown-content').forEach((element) => {
-                    if (element !== c) {
-                        element.classList.remove('mydropdown-show'); // hide other visible menu elements
-                    }
-                });
-            };
-            b.appendChild(document.createTextNode(name));
-            d.appendChild(b);
-            d.appendChild(c);
-            return d;
-        };
+  #buildNavbar(navbarHtmlElement) {
+    const createMenu = (name, elements) => {
+      const c = document.createElement('div');
+      c.setAttribute('class', 'mydropdown-content');
+      c.setAttribute('id', 'myDropdown');
 
-        const createIcon = (tooltip, icon, glyphName, click, width = 20, height = 20) => {
-            const a = document.createElement('a');
-            a.onclick = click;
+      elements.forEach((e) => {
+        const a = document.createElement('a');
+        a.appendChild(document.createTextNode(e.name));
+        a.onclick = e.click;
+        c.appendChild(a);
+      });
 
-            if (glyphName !== undefined) {
-                const i = document.createElement('i');
-                i.setAttribute("class", `glyphicon glyphicon-${glyphName}`);
-                a.appendChild(i);
-            } else if (icon !== undefined) {
-                const img = document.createElement('img');
-                img.setAttribute("src", icon);
-                img.setAttribute("width", width);
-                img.setAttribute("height", height);
-                a.appendChild(img);
-            }
+      const d = document.createElement('div');
+      d.setAttribute('class', 'mydropdown');
+      const b = document.createElement('button');
+      b.setAttribute('class', 'dropbtn');
+      b.onclick = function () {
+        c.classList.toggle('mydropdown-show');
+        document.querySelectorAll('.mydropdown-content').forEach((element) => {
+          if (element !== c) {
+            element.classList.remove('mydropdown-show'); // hide other visible menu elements
+          }
+        });
+      };
+      b.appendChild(document.createTextNode(name));
+      d.appendChild(b);
+      d.appendChild(c);
+      return d;
+    };
 
-            const t = document.createElement('span');
-            t.setAttribute("class", "mytooltiptext")
-            t.appendChild(document.createTextNode(tooltip));
+    const createIcon = (tooltip, icon, glyphName, click, width = 20, height = 20) => {
+      const a = document.createElement('a');
+      a.onclick = click;
 
-            a.appendChild(t);
-            a.setAttribute("class", "mytooltip");
-            return a;
-        };
+      if (glyphName !== undefined) {
+        const i = document.createElement('i');
+        i.setAttribute('class', `glyphicon glyphicon-${glyphName}`);
+        a.appendChild(i);
+      } else if (icon !== undefined) {
+        const img = document.createElement('img');
+        img.setAttribute('src', icon);
+        img.setAttribute('width', width);
+        img.setAttribute('height', height);
+        a.appendChild(img);
+      }
 
-        navbarHtmlElement.innerHTML = '';
-        this.#getMenus().forEach((m) => navbarHtmlElement.appendChild(createMenu(m.name, m.elements)));
-        this.#getIcons()
-        .forEach((i) => { navbarHtmlElement.appendChild(createIcon(i.tooltip, i.icon, i.glyphName, i.click, i.width === undefined ? 20 : i.width, i.height === undefined ? 20 : i.height )) });
-    }
+      const t = document.createElement('span');
+      t.setAttribute('class', 'mytooltiptext');
+      t.appendChild(document.createTextNode(tooltip));
+
+      a.appendChild(t);
+      a.setAttribute('class', 'mytooltip');
+      return a;
+    };
+
+    navbarHtmlElement.innerHTML = '';
+    this.#getMenus().forEach((m) => navbarHtmlElement.appendChild(createMenu(m.name, m.elements)));
+    this.#getIcons()
+      .forEach((i) => {
+        navbarHtmlElement.appendChild(
+          createIcon(
+            i.tooltip,
+            i.icon,
+            i.glyphName,
+            i.click,
+            i.width === undefined ? 20 : i.width,
+            i.height === undefined ? 20 : i.height
+          )
+        );
+      });
+  }
 }
 
 export { NavigationBar };
-
-
