4bc3702541894f5d4e34a8c73d84761ea6aedfae
Joe Meszaros <jmeszaros@hiya.com>
2025-01-08 19:56:27 +0100

    Format export.js

--- src/io/export.js
+++ src/io/export.js
@@ -1,31 +1,31 @@
 class Exporter {
 
-    static exportAsJson = (obj, filename) => {
-        const blob = new Blob([JSON.stringify(obj, null, 2)], {
-            type: 'application/json',
-        });
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = `${filename}.json`;
-        a.click();
-        URL.revokeObjectURL(url);
-    }
+  static exportAsJson = (obj, filename) => {
+    const blob = new Blob([JSON.stringify(obj, null, 2)], {
+      type : 'application/json'
+    });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = `${filename}.json`;
+    a.click();
+    URL.revokeObjectURL(url);
+  };
 
-    static exportCaves(caves) {
-        caves.forEach(cave => {
-            Exporter.exportAsJson(cave.toExport(), cave.name);
-        });
-    }
+  static exportCaves(caves) {
+    caves.forEach((cave) => {
+      Exporter.exportAsJson(cave.toExport(), cave.name);
+    });
+  }
 
-    static exportPNG(scene) {
-        scene.renderScene();
-        const base64 = scene.domElement.toDataURL('image/png');
-        let a = document.createElement('a'); // Create a temporary anchor.
-        a.href = base64;
-        a.download = 'export.png';
-        a.click();
-    }
+  static exportPNG(scene) {
+    scene.renderScene();
+    const base64 = scene.domElement.toDataURL('image/png');
+    let a = document.createElement('a'); // Create a temporary anchor.
+    a.href = base64;
+    a.download = 'export.png';
+    a.click();
+  }
 }
 
 export { Exporter };
